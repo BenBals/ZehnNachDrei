0d379b0e059ec051593025c8aedf8027354ab78d
Ben <benjustusbals@gmail.com>
2015-10-20 20:33:36 +0200

    resources folder, mobile config

--- mobile-config.js
+++ mobile-config.js
@@ -1,19 +1,35 @@
-App.accessRule('*')
+App.accessRule('*');
 
 App.info({
   name: 'ZehnNachDrei',
+  id: 'de.zehnnachdrei.zehnnachdrei',
   description: 'Die App der Schülerzeitung des CJD Rostock.',
+  author: 'ZehnNachDrei',
+  email: 'zehnnachdrei@cjd-rostock.mv.lo-net2.de',
+  website: 'http://zehnnachdrei.de',
   version: '0.1.2'
-})
+});
 
 App.icons({
-  'iphone': 'public/icon/Icon-60@2x@0,5x.png',
-  'iphone_2x': 'public/icon/Icon-60@2x.png'
+  'iphone': 'resources/icon/Icon-60@2x@0,5x.png',
+  'iphone_2x': 'resources/icon/Icon-60@2x.png',
+  'iphone_3x': 'resources/icon/Icon-60@3x.png',
+  'ipad': 'resources/icon/Icon-76.png',
+  'ipad_2x': 'resources/icon/Icon-76@2x.png'
+
 });
 
 
-// App.launchScreens({
-//   'iphone': 'resources/splash/Default~iphone.png',
-//   'iphone_2x': 'resources/splash/Default@2x~iphone.png',
-//   'iphone5': 'resources/splash/Default-568h@2x~iphone.png'
-// });
+App.launchScreens({
+  'iphone': 'resources/splash/iPhone4.png',
+  'iphone_2x': 'resources/splash/iPhone4@2x.png',
+  'iphone5': 'resources/splash/iPhone5.png',
+  'iphone6': 'resources/splash/iPhone6.png',
+  'iphone6p_portrait': 'resources/splash/iPhone6plus-Portrait.png',
+  'iphone6p_landscape': 'resources/splash/iPhone6plus-Landscape.png',
+
+  'ipad_portrait': 'resources/splash/iPad-Portrait.png',
+  'ipad_portrait_2x': 'resources/splash/iPad-Portrait@2x.png',
+  'ipad_landscape': 'resources/splash/iPad-Landscape.png',
+  'ipad_landscape_2x': 'resources/splash/iPad-Landscape@2x.png'
+});
