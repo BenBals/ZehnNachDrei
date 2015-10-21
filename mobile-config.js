956979a0f01360e85f320921f45c5299e28f2f88
Ben <benjustusbals@gmail.com>
2015-10-21 20:43:24 +0200

    android icons and stuff

--- mobile-config.js
+++ mobile-config.js
@@ -7,7 +7,7 @@ App.info({
   author: 'ZehnNachDrei',
   email: 'zehnnachdrei@cjd-rostock.mv.lo-net2.de',
   website: 'http://zehnnachdrei.de',
-  version: '0.1.6'
+  version: '0.1.7'
 });
 
 App.icons({
@@ -15,8 +15,12 @@ App.icons({
   'iphone_2x': 'resources/icon/Icon-60@2x.png',
   'iphone_3x': 'resources/icon/Icon-60@3x.png',
   'ipad': 'resources/icon/Icon-76.png',
-  'ipad_2x': 'resources/icon/Icon-76@2x.png'
+  'ipad_2x': 'resources/icon/Icon-76@2x.png',
 
+  'android_ldpi': 'resources/icon/android_ldpi.png',
+  'android_mdpi': 'resources/icon/android_mdpi.png',
+  'android_hdpi': 'resources/icon/android_hdpi.png',
+  'android_xhdpi': 'resources/icon/android_xhdpi.png'
 });
 
 
@@ -32,6 +36,16 @@ App.launchScreens({
   'ipad_portrait_2x': 'resources/splash/iPad-Portrait@2x.png',
   'ipad_landscape': 'resources/splash/iPad-Landscape.png',
   'ipad_landscape_2x': 'resources/splash/iPad-Landscape@2x.png'
+
+  'android_ldpi_portrait': 'resources/splash/android_ldpi_portrait.png'
+  'android_ldpi_landscape': 'resources/splash/android_ldpi_landscape.png'
+  'android_mdpi_portrait': 'resources/splash/android_mdpi_portrait.png'
+  'android_mdpi_landscape': 'resources/splash/android_mdpi_landscape.png'
+  'android_hdpi_portrait': 'resources/splash/android_hdpi_portrait.png'
+  'android_hdpi_landscape': 'resources/splash/android_hdpi_landscape.png'
+  'android_xhdpi_portrait': 'resources/splash/android_xhdpi_portrait.png'
+  'android_xhdpi_landscape': 'resources/splash/android_xhdpi_landscape.png'
+
 });
 
 App.setPreference('StatusBarOverlaysWebView', 'false');
